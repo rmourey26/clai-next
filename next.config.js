836307a80a7881dcc666e3d021a18cf4b4add2e1
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-03-12 17:03:16 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/rmourey26/clai-next",
-        permanent: false,
-      },
-      {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/clai-next",
-        permanent: false,
-      },
-    ];
-  },
+  reactStrictMode: true
 };
