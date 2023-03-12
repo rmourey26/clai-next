cec3116e134a63a131ae98a7e5db27a552b373a4
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-03-11 23:38:24 -0500

    update

---
+++ pages/index.tsx
@@ -0,0 +1,193 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import DropDown, { VibeType } from "../components/DropDown";
+import Footer from "../components/Footer";
+import Github from "../components/GitHub";
+import Header from "../components/Header";
+import LoadingDots from "../components/LoadingDots";
+
+const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [letter, setLetter] = useState("");
+  const [vibe, setVibe] = useState<VibeType>("Professional");
+  const [generatedLetters, setGeneratedLetters] = useState<String>("");
+
+  const letterRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToLetters = () => {
+    if (letterRef.current !== null) {
+      letterRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const prompt = `Generate 2 ${vibe} job application cover letters with no hashtags and clearly labeled "1." and "2.". ${
+    vibe === "Funny"
+      ? "Make sure there is a joke in there and it's a little ridiculous."
+      : null
+  }
+      Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${letter}${
+    letter.slice(-1) === "." ? "" : "."
+  }`;
+
+  const generateLetter = async (e: any) => {
+    e.preventDefault();
+    setGeneratedLetters("");
+    setLoading(true);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setGeneratedLetters((prev) => prev + chunkValue);
+    }
+    scrollToLetters();
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head>
+        <title>CoverLetterAI</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+        <a
+          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
+          href="https://github.com/rmourey26/clai-next"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+          <p>Star on GitHub</p>
+        </a>
+        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
+          Create an AI generated cover letter in seconds
+        </h1>
+        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
+        <div className="max-w-xl w-full">
+          <div className="flex mt-10 items-center space-x-3">
+            <Image
+              src="/1-black.png"
+              width={30}
+              height={30}
+              alt="1 icon"
+              className="mb-5 sm:mb-0"
+            />
+            <p className="text-left font-medium">
+              Copy your current letter{" "}
+              <span className="text-slate-500">
+                (or write a few sentences about yourself)
+              </span>
+              .
+            </p>
+          </div>
+          <textarea
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
+            }
+          />
+          <div className="flex mb-5 items-center space-x-3">
+            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
+            <p className="text-left font-medium">Select your vibe.</p>
+          </div>
+          <div className="block">
+            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
+          </div>
+
+          {!loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              onClick={(e) => generateLetter(e)}
+            >
+              Generate your cover letter &rarr;
+            </button>
+          )}
+          {loading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              disabled
+            >
+              <LoadingDots color="white" style="large" />
+            </button>
+          )}
+        </div>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{ duration: 2000 }}
+        />
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+        <div className="space-y-10 my-10">
+          {generatedLetters && (
+            <>
+              <div>
+                <h2
+                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
+                  ref={letterRef}
+                >
+                  Your generated letters
+                </h2>
+              </div>
+              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                {generatedLetters
+                  .substring(generatedLetters.indexOf("1") + 3)
+                  .split("2.")
+                  .map((generatedLetter) => {
+                    return (
+                      <div
+                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                        onClick={() => {
+                          navigator.clipboard.writeText(generatedLetter);
+                          toast("Bio copied to clipboard", {
+                            icon: "✂️",
+                          });
+                        }}
+                        key={generatedLetter}
+                      >
+                        <p>{generatedLetter}</p>
+                      </div>
+                    );
+                  })}
+              </div>
+            </>
+          )}
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
